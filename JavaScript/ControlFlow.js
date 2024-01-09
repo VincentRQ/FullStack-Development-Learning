d54893c0dfb73d381d9541a3773c520f22502afc
VincentRQ <VRQ20@fsu.edu>
2024-01-08 19:14:40 -0500

    JavaScript Learning
    Worked on Primitives, variables, and Control Flow - Conditions and Logical Operators

---
+++ JavaScript/ControlFlow.js
@@ -0,0 +1,11 @@
+//Control Flow is the order in which the computer executes statements in a script.
+
+const name = 'Laura'
+
+if (name === 'Vincent') {
+  alert('Hi Vincent')
+} else if (name === 'Laura') {
+  alert('Who are you?')
+} else 'What is your name?'
+
+console.log(name)
--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -8,4 +8,30 @@ var name = 'Joever'
 var number = 33
 var sum = name + number
 // console.log(sum)
-var name = prompt('What is your name?')
+// var firstName = prompt('Enter your First Name')
+// var lastName = prompt('Enter your Last Name')
+// var Name = firstName + lastName
+
+// var day = prompt('Enter the Day of your Birth')
+// var month = prompt('Enter the Month of your Birth')
+// var year = prompt('Enter the Year of your Birth')
+// var BirthDay = day + month + year
+
+// var day1 = prompt('Distance on Day-1 in kilometers')
+// var day2 = prompt('Distance on Day-2 in kilometers')
+// var day3 = prompt('Distance on Day-3 in kilometers')
+
+// var SumofDays = Number(day1) + Number(day2) + Number(day3)
+
+// alert('Distance' + SumofDays + 'km')
+
+// var x = false
+// x = true
+// x = 10
+// console.log(x)
+
+// var email;
+// console.log(email);
+
+let age = 33
+age = 33
