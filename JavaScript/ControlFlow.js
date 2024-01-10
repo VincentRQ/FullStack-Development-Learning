cccd3bb5f14b0c8d492bc171310d672aa3630ff8
VincentRQ <VRQ20@fsu.edu>
2024-01-09 22:52:58 -0500

    Update ControlFlow.js
    Minor Update

--- JavaScript/ControlFlow.js
+++ JavaScript/ControlFlow.js
@@ -1,11 +1,14 @@
 //Control Flow is the order in which the computer executes statements in a script.
 
-const name = 'Laura'
+// const name = 'Laura'
 
-if (name === 'Vincent') {
-  alert('Hi Vincent')
-} else if (name === 'Laura') {
-  alert('Who are you?')
-} else 'What is your name?'
+// if (name === 'Vincent') {
+//   alert('Hi Vincent')
+// } else if (name === 'Laura') {
+//   alert('Hi Laura')
+// } else 'What is your name?'
 
-console.log(name)
+// console.log(name)
+
+// There are conditions, which use functions like if, else, else if, etc.
+// There are also logical operators, such as ||, &&, and ! (not)
