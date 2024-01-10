e619c0311441eee96f8550ba4002c3b21593dc89
VincentRQ <VRQ20@fsu.edu>
2024-01-09 23:52:33 -0500

    Object Literals

--- JavaScript/ControlFlow.js
+++ JavaScript/ControlFlow.js
@@ -12,3 +12,20 @@
 
 // There are conditions, which use functions like if, else, else if, etc.
 // There are also logical operators, such as ||, &&, and ! (not)
+
+const m = 'Hi there'
+console.log(m.substring(0, 5).toupperCase())
+
+let str = 'Hello, World!'
+console.log(str.charAt(0)) // Outputs: "H"
+
+let str1 = 'Hello'
+let str2 = ', World!'
+console.log(str1.concat(str2)) // Outputs: "Hello, World!"
+
+let str = 'Hello, World!'
+console.log(str.toUpperCase()) // Outputs: "HELLO, WORLD!"
+console.log(str.toLowerCase()) // Outputs: "hello, world!"
+
+let str = '   Hello, World!   '
+console.log(str.trim()) // Outputs: "Hello, World!"
