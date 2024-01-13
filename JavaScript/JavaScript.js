da9d3d5c281ac12398bb8f43bcc01034fb29b754
VincentRQ <VRQ20@fsu.edu>
2024-01-12 19:03:16 -0500

    Update JavaScript.js
    Worked on Functions & Constructor Functions within javascript. Learned Function efficiency, arrow functions, function anonymity, as well as the process to create a function.

--- JavaScript/JavaScript.js
+++ JavaScript/JavaScript.js
@@ -83,23 +83,23 @@
 
 //Array Loop
 
-const CheckList = [
-  {
-    id: 1,
-    task: 'Do 10 pushups',
-    Completed: true,
-  },
-  {
-    id: 2,
-    task: 'Do 10 pullups',
-    Completed: true,
-  },
-  {
-    id: 3,
-    task: 'Do 10 squats',
-    Completed: false,
-  },
-]
+// const CheckList = [
+//   {
+//     id: 1,
+//     task: 'Do 10 pushups',
+//     Completed: true,
+//   },
+//   {
+//     id: 2,
+//     task: 'Do 10 pullups',
+//     Completed: true,
+//   },
+//   {
+//     id: 3,
+//     task: 'Do 10 squats',
+//     Completed: false,
+//   },
+// ]
 
 // for (j = 0; j < CheckList.length; j++) {
 //   console.log(CheckList[j].task)
@@ -154,16 +154,99 @@ const CheckList = [
 
 // console.log(loopCompleted)
 
-// Array (remember, they start with 0) (You can have different data types in array)
+// // Array (remember, they start with 0) (You can have different data types in array)
 
-const cars = ['Audi', 'BMW', 20, true]
-// cars[4] = 'Ford'
-cars.push('Toyota')
-cars.unshift('Toyota')
-//Removes from array
-cars.shift()
-cars.pop()
+// const cars = ['Audi', 'BMW', 20, true]
+// // cars[4] = 'Ford'
+// cars.push('Toyota')
+// cars.unshift('Toyota')
+// //Removes from array
+// cars.shift()
+// cars.pop()
 
-// console.log(cars)
-// console.log(Array.isArray(cars))
-console.log(cars.indexOf('BMW'))
+// // console.log(cars)
+// // console.log(Array.isArray(cars))
+// console.log(cars.indexOf('BMW'))
+
+// //Arrays cannot be reassigned
+
+//-----------------------------------------------------------
+//Functions
+//cole log, alerts, prommpts
+
+//How to create functions
+// function (argument) (code)
+// console.log('Hi', 'Norbert')
+
+// function myFunction() {
+//   console.log(
+//     'This is my Function',
+//     'There are many functions like this',
+//     'But this one is mind!'
+//   )
+// }
+
+// myFunction()
+
+//Function Expression (Anonymous Function)
+
+// const mySecondFunction=function() {
+//   console.log('Function the II');
+
+// }
+// mySecondFunction
+
+//Function Efficiency
+
+// function code(){
+//   console.log('eat')
+//   console.log('sleep')
+//   console.log('code')
+//   console.log('repeat')
+// }
+
+// code()
+
+// function efficientFunction(NoRepeat){
+//   console.log(NoRepeat)
+// }
+// efficientFunction('sleep')
+
+// function Nums(num1, num2) {
+//   return num1 + num2
+// }
+// console.log(Nums(5, 10))
+
+//Arrow Functions
+
+// const Nums = (num1, num2) => num1 / num2
+// console.log(Nums(100, 10))
+
+//------------------------------------------------------------------------------
+//Object Oriented Programming
+//OOP helps us construct objects using constructor functions
+//Two ways of doing this: prototypes & ES6 classes
+//Constructor functions begin with a capital letter
+//Constructor Function
+
+function User(firstName, lastName, Email, DoB) {
+  this.firstName = firstName
+  this.lastName = lastName
+  this.Email = Email
+  this.DoB = new Date(DoB)
+  // add a New Method/Function
+  this.getBirthYear = function () {
+    return this.DoB.getFullYear()
+  }
+  this.getFullName = function () {
+    return '${this.firstName}'
+  }
+}
+
+//We create a variable, then said that the user should be assigned to a variable, with a specific string of arguments
+const user1 = new User('Norbert', 'Menyhart', 'noEmail@mail.com', '02/19/2004')
+const user2 = new User('Quintin', 'Menyhart', 'no2Email@mail.com', '09/09/2004')
+console.log(user1.DoB.getFullYear)
+console.log(user2.getBirthYear())
+
+//Prototypes
