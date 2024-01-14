9b5ecfa9946fd8891aeafc877e5b2e0dc4e924ce
VincentRQ <VRQ20@fsu.edu>
2024-01-13 23:58:42 -0500

    Minor Update

--- JavaScript/The DOM/dom.js
+++ JavaScript/The DOM/dom.js
@@ -2,3 +2,11 @@
 console.dir(document)
 
 console.log(document.URL)
+console.log(document.domain)
+console.log(document.title)
+console.log(document.body)
+document.title = 'The Title'
+console.log(document.doctype)
+
+console.log(document.all)
+console.log(document.all[`14`])
