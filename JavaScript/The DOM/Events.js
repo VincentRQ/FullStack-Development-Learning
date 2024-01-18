3dcf3a4389b87de0f94ce921db51bca32078eb84
VincentRQ <VRQ20@fsu.edu>
2024-01-17 20:28:47 -0500

    Update to JS
    Worked with additional DOM Events

--- JavaScript/The DOM/Events.js
+++ JavaScript/The DOM/Events.js
@@ -3,11 +3,11 @@
 // const button = document.getElementById('btn')
 // .addEventListener('click', function(){
 //     console.log("I have been clicked :D")
-// })
+// // })
 
-const button = document
-  .getElementById('btn')
-  .addEventListener('click', buttonClick)
+// const button = document
+//   .getElementById('btn')
+//   .addEventListener('click', buttonClick)
 
 //Basically, we are adding a function, so that when you keep clicking the button, parts of the page will change.
 // function buttonClick(){
@@ -33,4 +33,24 @@ function buttonClick(ep) {
   // console.log(ep.shiftKey)
   // console.log(ep.altKey)
   // console.log(ep.altKey + ep.shiftKey)
+}
+
+// DOM Events Part 2
+// Mouse Events
+const button = document.getElementById('btn')
+const circle = document.getElementById('circle')
+
+// button.addEventListener('click', initEvent)
+// button.addEventListener('dblclick', initEvent)
+// button.addEventListener('mousedown', initEvent)
+// button.addEventListener('mouseup', initEvent)
+
+circle.addEventListener('mouseover', initEvent)
+circle.addEventListener('mouseout', initEvent)
+circle.addEventListener('mouseenter', initEvent)
+circle.addEventListener('mouseleave', initEvent)
+
+function initEvent(ep) {
+  console.log('EVENT TYPE' + ep.type)
+  circle.style.backgroundColor = 'rgb("+ep.offsetX+","+ep.offsetY+", 30)'
 }
